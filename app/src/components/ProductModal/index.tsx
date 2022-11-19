4b759b17a4a5a7f275684b1626df522ed479cb0d
Augusto Polonio <augustodrp@gmail.com>
2022-11-18 23:54:17 -0300

    Mobile: Product modal

--- app/src/components/ProductModal/index.tsx
+++ app/src/components/ProductModal/index.tsx
@@ -1,8 +1,10 @@
 import { FlatList, Modal } from "react-native";
 import { Product } from "../../types/Product";
+import { formatCurrency } from "../../utils/formatCurrency";
+import { Button } from "../Button";
 import { Close } from "../Icons/Close";
 import { Text } from "../Text";
-import { CloseButton, Header, Image, Ingredient, IngredientsContainer, ModalBody } from "./styles";
+import { CloseButton, Footer, FooterContainer, Header, Image, Ingredient, IngredientsContainer, ModalBody, PriceContainer } from "./styles";
 
 interface ProductModalProps {
   visible: boolean
@@ -43,29 +45,43 @@ export function ProductModal({ visible, onClose, product }: ProductModalProps) {
           </Text>
         </Header>
 
-        <IngredientsContainer>
-          <Text weight="600" color="#666">
-            Ingredientes
-          </Text>
+        {product.ingredients.length > 0 && (
+          <IngredientsContainer>
+            <Text weight="600" color="#666">
+              Ingredientes
+            </Text>
 
-          <FlatList
-            data={product.ingredients}
-            keyExtractor={ingredient => ingredient._id}
-            showsVerticalScrollIndicator={false}
-            style={{ marginTop: 16 }}
-            renderItem={({ item: ingredient }) => (
-              <Ingredient>
-                <Text>{ingredient.icon}</Text>
-                <Text size={14} color="#666" style={{ marginLeft: 20 }}>
-                  {ingredient.name}
-                </Text>
-              </Ingredient>
-            )}
-          />
-
-        </IngredientsContainer>
+            <FlatList
+              data={product.ingredients}
+              keyExtractor={ingredient => ingredient._id}
+              showsVerticalScrollIndicator={false}
+              style={{ marginTop: 16 }}
+              renderItem={({ item: ingredient }) => (
+                <Ingredient>
+                  <Text>{ingredient.icon}</Text>
+                  <Text size={14} color="#666" style={{ marginLeft: 20 }}>
+                    {ingredient.name}
+                  </Text>
+                </Ingredient>
+              )}
+            />
+          </IngredientsContainer>
+        )}
 
       </ModalBody>
+
+      <Footer>
+        <FooterContainer>
+          <PriceContainer>
+            <Text>Preço</Text>
+            <Text size={20} weight="600">{formatCurrency(product.price)}</Text>
+          </PriceContainer>
+
+          <Button onPress={() => alert('Adicionar ao pedido')}>
+            Adicionar ao pedido
+          </Button>
+        </FooterContainer>
+      </Footer>
 
     </Modal>
   );
--- app/src/components/ProductModal/styles.ts
+++ app/src/components/ProductModal/styles.ts
@@ -27,7 +27,8 @@ export const Header = styled.View`
 `;
 
 export const IngredientsContainer = styled.View`
-  margin-top: 23px;
+  margin-top: 32px;
+  flex: 1;
 `;
 
 export const Ingredient = styled.View`
@@ -37,4 +38,20 @@ export const Ingredient = styled.View`
   flex-direction: row;
   padding: 16px;
   margin-bottom: 4px;
+`;
+
+export const Footer = styled.View`
+  min-height: 110px;
+  background: #fff;
+  padding: 16px 24px;
+`;
+
+export const FooterContainer = styled.SafeAreaView`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const PriceContainer = styled.SafeAreaView`
+
 `;
