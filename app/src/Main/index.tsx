68abf8eb8d3412e444abca1103c8dc121d0f1fa7
Augusto Polonio <augustodrp@gmail.com>
2022-11-19 15:45:09 -0300

    Mobile: Empty cart interface

--- app/src/Main/index.tsx
+++ app/src/Main/index.tsx
@@ -13,14 +13,14 @@ export function Main() {
   const [isTableModalVisible, setIsTableModalVisible] = useState(false);
   const [selectedTable, setSelectedTable] = useState('');
   const [cartItems, setCartItems] = useState<CartItem[]>([
-    {
-      quantity: 1,
-      product: products[0]
-    },
-    {
-      quantity: 2,
-      product: products[1]
-    },
+    // {
+    //   quantity: 1,
+    //   product: products[0]
+    // },
+    // {
+    //   quantity: 2,
+    //   product: products[1]
+    // },
     // {
     //   quantity: 2,
     //   product: products[2]
