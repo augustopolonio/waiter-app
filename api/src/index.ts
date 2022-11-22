5bded0169ae3b60d95065f80b03d016b85ad3cf1
augustodrp <augustodrp@gmail.com>
2022-11-21 22:27:13 -0300

    WebSocket: Installed using the cmd yarn add socket.io in server side (api) and set the basic configs

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,14 +1,23 @@
 import path from 'node:path';
+import http from 'node:http';
 import express from 'express';
 import mongose from 'mongoose';
 import { router } from './router';
+import { Server } from 'socket.io';
 
 
 
 mongose.connect('mongodb://127.0.0.1:27017')
   .then(() => {
-    const app = express();
     const port = 3001;
+
+    const app = express();
+    const server = http.createServer(app);
+    const io = new Server(server);
+
+    io.on('connect', () => {
+      console.log('Conectou!');
+    });
 
     app.use((req, res, next) => {
       res.setHeader('Access-Control-Allow-Origin', '*'); //http://192.168.2.103:3001
@@ -21,7 +30,7 @@ mongose.connect('mongodb://127.0.0.1:27017')
     app.use(express.json());
     app.use(router);
 
-    app.listen(port, () => {
+    server.listen(port, () => {
       console.log(`🌎 Server is running on http://localhost:${port}`);
     });
   })
